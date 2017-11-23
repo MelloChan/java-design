3a8d2adcdf52ba170b786e9f67c70e18cb35fc0b
MelloYuki <352983198@qq.com>
2017-11-23 23:06:09 +0800

    update 适配器模式 门面模式

---
+++ design/exam07/facade/Screen.java
@@ -0,0 +1,22 @@
+package design.exam07.facade;
+
+public class Screen {
+	private String description;
+	
+	public Screen(String description) {
+		this.description = description;
+	}
+ 
+	public void up() {
+		System.out.println(description + " going up");
+	}
+ 
+	public void down() {
+		System.out.println(description + " going down");
+	}
+ 
+  
+        public String toString() {
+                return description;
+        }
+}
